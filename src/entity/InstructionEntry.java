2b8a7ceb0671d6376121a24db10c52fb5876a9b5
McKrispy <gwaangitjan@outlook.com>
2025-06-15 17:44:08 +0200

    Merge_updates_from_PR_ONLY_SRC

--- src/entity/InstructionEntry.java
+++ src/entity/InstructionEntry.java
@@ -1,22 +1,23 @@
 package entity;
 //store how much ingredients is needed e.g. 400 grams beef
 public class InstructionEntry {
+	private int recipeId;
     private Ingredient ingredient;
     private double quantity;
     private String unit;
     
     //constructor
 	public InstructionEntry(Ingredient ingredient, double quantity, String unit) {
-		super();
 		this.ingredient = ingredient;
 		this.quantity = quantity;
 		this.unit = unit;
 	}
-	
-	
+
+		
 	public void scaleQuantity(double ratio) {
 	    this.quantity *= ratio;
 	}
+
 
 	//getters&setters
 	public Ingredient getIngredient() {
@@ -43,6 +44,10 @@ public class InstructionEntry {
 		this.unit = unit;
 	}
 
+	public int getRecipeId() {
+		return recipeId;
+	}
+	
 	@Override
 	public String toString() {
 	    return quantity + " " + unit + " " + ingredient.getName();
